e4d18725fc9374c4a9163369a48bec8ec5edc200
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-18 20:16:07 +0200

    feat: introduce rules

--- lsp/src/configuration.rs
+++ lsp/src/configuration.rs
@@ -19,6 +19,39 @@
 
 use serde_json::Value;
 
+#[derive(Debug, PartialEq)]
+pub enum RulesMode {
+    Whitelist,
+    Blacklist,
+}
+
+#[derive(Debug)]
+pub struct Rules {
+    pub mode: RulesMode,
+    pub paths: Vec<String>,
+}
+
+impl Default for Rules {
+    fn default() -> Self {
+        Rules {
+            mode: RulesMode::Blacklist,
+            paths: Vec::new(),
+        }
+    }
+}
+
+impl Rules {
+    pub fn suitable(&self, path: &str) -> bool {
+        let contains = self.paths.contains(&path.to_string());
+
+        if self.mode == RulesMode::Blacklist {
+            !contains
+        } else {
+            contains
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Configuration {
     pub base_icons_url: String,
@@ -30,6 +63,8 @@ pub struct Configuration {
     pub large_text: Option<String>,
     pub small_image: Option<String>,
     pub small_text: Option<String>,
+
+    pub rules: Rules,
 
     pub git_integration: bool,
 }
@@ -66,6 +101,7 @@ impl Configuration {
             large_text: Some(String::from("{language:u}")),
             small_image: Some(String::from("{base_icons_url}/zed.png")),
             small_text: Some(String::from("Zed")),
+            rules: Rules::default(),
             git_integration: true,
         }
     }
@@ -79,6 +115,28 @@ impl Configuration {
             set_option!(self, options, large_text, "large_text");
             set_option!(self, options, small_image, "small_image");
             set_option!(self, options, small_text, "small_text");
+
+            if let Some(rules) = options.get("rules") {
+                self.rules.mode = rules.get("mode").and_then(|m| m.as_str()).map_or(
+                    RulesMode::Blacklist,
+                    |mode| match mode {
+                        "whitelist" => RulesMode::Whitelist,
+                        "blacklist" => RulesMode::Blacklist,
+                        _ => RulesMode::Blacklist,
+                    },
+                );
+
+                self.rules.paths =
+                    rules
+                        .get("paths")
+                        .and_then(|p| p.as_array())
+                        .map_or(Vec::new(), |paths| {
+                            paths
+                                .iter()
+                                .filter_map(|p| p.as_str().map(|s| s.to_string()))
+                                .collect()
+                        });
+            }
 
             if let Some(git_integration) = options.get("git_integration") {
                 self.git_integration = git_integration.as_bool().unwrap_or(true);
