dd450550a1cadec7d451fdd185b863dab518dc15
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-02-24 19:59:25 +0100

    style: remove unused imports

--- src/discord_presence.rs
+++ src/discord_presence.rs
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-use std::{fmt::format, fs};
+use std::fs;
 use zed_extension_api::{self as zed};
 
 struct DiscordPresenceExtension {
